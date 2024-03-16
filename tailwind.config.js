33f2fd843c9b0a33d702ff93b239f84edd480d38
eaglefluke21 <yadavankit7fc4@gmail.com>
2024-03-16 17:38:31 +0530

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       fontFamily: {
         anta: ['Anta', 'sans-serif'], 
       },
-
+      backgroundImage: {
+        'two-tone': 'linear-gradient(-60deg, rgba(229,231,235,0) 50%,  rgb(216 ,180, 254 ,1) 50%)',
+      },
+      
+     
     },
   },
   plugins: [],
