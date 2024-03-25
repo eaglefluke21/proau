3be4c6c725b55a419c6ff8baa930498935c960d0
eaglefluke21 <yadavankit7fc4@gmail.com>
2024-03-25 11:24:31 +0530

    Need to Deploy

--- server/index.js
+++ server/index.js
@@ -7,21 +7,23 @@ import dashbackendroute from './dashbackend.js';
 
 import cors from 'cors';
 
+
+
 const app = express();
 const PORT = process.env.PORT || 3300;
 
 // Middleware
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cors());
+
 
 // backend.js
 app.use('', backendroute);
 
 // dashbackend.js
-
 app.use('', dashbackendroute);
+
 
 // Starting the server
 app.listen(PORT, () => {
